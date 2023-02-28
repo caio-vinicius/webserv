44b9ac7647584283b55072f13c6cc85c26c948f0
Luigi Ferrari <luigienzopereiraferrari@gmail.com>
2023-02-28 18:44:12 -0300

    add: config data structure

--- Config.hpp
+++ Config.hpp
@@ -1,13 +1,43 @@
 /* Copyright (c) 2023 Caio Souza, Guilherme Martinelli, Luigi Ferrari. */
 /* All rights reserved. 42 */
 
+#ifndef CONFIG_HPP_
+#define CONFIG_HPP_
+
+#include <string>
+#include <map>
+
+typedef std::string string;
 namespace ft {
-    class Config {
-        public:
-            Config(void);
-            Config(std::string const filename);
-            ~Config(void);
-        private:
-            std::string _filename;
-    }; // class Config
-} // namespace ft
+
+class Config {
+ private:
+    class Location {
+     private:
+        std::map<string, string> params;
+     public:
+        Location(void) {}
+    };
+
+    class Server {
+     private:
+        std::map<string, string> params;
+        std::map<string, Location> location;
+     public:
+        Server(void) {}
+    };
+
+    std::map<string, string> params;
+    std::map<string, Server> server;
+    std::string _filename;
+
+ public:
+    Config(void) {}
+    explicit Config(std::string const filename): _filename(filename) {}
+    void addServer(void) {}
+    ~Config(void) {}
+};  // class Config
+
+}  // namespace ft
+
+#endif  // CONFIG_HPP_
