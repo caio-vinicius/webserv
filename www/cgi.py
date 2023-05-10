934bf4e989c06684320aba9a4c053cb0dd1a2aa7
Guilherme Martinelli <guilhermefmartinelli@gmail.com>
2023-05-10 20:50:31 -0300

    cgi: name salute

--- www/cgi.py
+++ www/cgi.py
@@ -3,9 +3,8 @@
 import os
 import sys
 
-print("Content-Type: text/html\r\n\r\n")
-
 name = os.environ['QUERY_STRING']
-print(f"<html><head><title>Hello, {name}!</title></head><body>")
-print("<h1>Hello, World!</h1>")
+str_list = name.split("=")
+print("<html><head><title>Hello!</title></head><body>")
+print(f"<h1>Hello, {str_list[1]}!</h1>")
 print("</body></html>")
